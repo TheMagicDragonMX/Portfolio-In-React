67c84826f4519e15e819e607d4738cb9f5eb084c
TheMagicDragonMX <turbojet901@gmail.com>
2022-11-22 21:57:49 -0600

    Testing background with squared divs

--- src/pages/Home/components/Map/Map.tsx
+++ src/pages/Home/components/Map/Map.tsx
@@ -1,10 +1,5 @@
-/* eslint-disable react/no-unknown-property */
-
 import React from "react"
 import "./Map.scss"
-
-import { Canvas } from "@react-three/fiber"
-import { OrbitControls } from "@react-three/drei"
 
 import { MapBackground } from "./MapBackground"
 
@@ -12,6 +7,8 @@ const Map : React.FC = () => {
 	return <>
 		<div className="map">
 			<MapBackground />
+
+			
 		</div>
 	</>
 }
--- src/pages/Home/components/Map/MapBackground/MapBackground.tsx
+++ src/pages/Home/components/Map/MapBackground/MapBackground.tsx
@@ -3,9 +3,29 @@ import "./MapBackground.scss"
 
 const MapBackground : React.FC = () => {
 
+	const getRandomGrayColor = () => {
+		const gray = Math.floor( Math.random() * 20 ) + 10
+		return `rgb(${ gray }, ${ gray }, ${ gray })`
+	}
+
+	const columns = 150
+	const rows = 100
+	const squares = new Array<string>(columns).fill( "" )
+	const groups = new Array<string>(rows).fill( "" )
+
 	return <>
 		<div className="map-background">
-			
+			{
+				groups.map( (group, key) => 
+					<div key={ key }>
+						{ 
+							squares.map( (square, key) => 
+								<div key={ key } className="little-square" style={{ backgroundColor: getRandomGrayColor() }}></div> 
+							)
+						}
+					</div>
+				)
+			}
 		</div>
 	</>
 }
