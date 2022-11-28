64fb613a0f78a2877e2e406753b2fef59d7c6157
TheMagicDragonMX <turbojet901@gmail.com>
2022-11-28 10:37:15 -0600

    Created basic puck design

--- src/pages/Home/components/Map/Puck/Puck.tsx
+++ src/pages/Home/components/Map/Puck/Puck.tsx
@@ -1,12 +1,22 @@
 import React from "react"
 import "./Puck.scss"
 
+import pfp from "@/assets/pfp-cubes.jpg"
+
 export interface PuckInterface {}
 
 const Puck : React.FC<PuckInterface> = () => {
 	return <>
 		<div className="puck">
-			
+			<span className="front-face">
+				<img src={ pfp } />
+			</span>
+
+			<span className="back-face"></span>
+			<span className="short-side" style={{ "--shortSide": 1 } as React.CSSProperties}></span>
+			<span className="short-side" style={{ "--shortSide": -1 } as React.CSSProperties}></span>
+			<span className="long-side" style={{ "--longSide": 1 } as React.CSSProperties}></span>
+			<span className="long-side" style={{ "--longSide": -1 } as React.CSSProperties}></span>
 		</div>
 	</>
 }
