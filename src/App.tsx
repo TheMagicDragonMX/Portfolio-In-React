057037f470115fc19f5e2530beb32e36a7401fc9
Alejandro David García González <88684972+TheMagicDragonMX@users.noreply.github.com>
2023-01-05 15:53:12 -0600

    Removed react router for now

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,11 @@
 import React from "react"
 import "./App.scss"
 
-import { Routes, Route } from "react-router-dom"
 import { Home } from "./pages"
-
 
 function App () {
 	return <>
-		<Routes>
-			<Route index element={ <Home/> } />
-
-			<Route path="home" element={ <Home /> } />
-			
-			<Route path="*" element={ <h1>Nothing! <i>for now...</i></h1> } />
-		</Routes>
+		<Home />
 	</>
 }
 
