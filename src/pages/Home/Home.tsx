87f0e1c4c3009523ff7c56249d79b2167d5f4307
Alejandro David García González <88684972+TheMagicDragonMX@users.noreply.github.com>
2023-01-08 12:18:49 -0600

    Changed name from "container" to "map"

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -7,7 +7,7 @@ const Home : React.FC = () => {
 	
 	return <>
 		<div className="home">
-			<div className="map-container">
+			<div className="map">
 				<MapBackground />
 				<SparkEnvironment />
 			</div>
