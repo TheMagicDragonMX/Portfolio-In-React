4020cc2495c8f981929ed49b9084a587e0e86ede
Alejandro David García González <88684972+TheMagicDragonMX@users.noreply.github.com>
2023-01-08 12:51:29 -0600

    Added scrolling control with mouse wheel

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -8,24 +8,57 @@ const SCROLLING_SPEED = 0.05
 const Home : React.FC = () => {
 	
 	const map = useRef<HTMLDivElement>(null)
-	const mapDisplacement = useRef(0)
-	const mapOffset = useRef(0)
+	const home = useRef<HTMLDivElement>(null)
+
+	const topXScrollLimit = useRef(0)
+	const topYScrollLimit = useRef(0)
+
+	const mapXDisplacement = useRef(0)
+	const mapXOffset = useRef(0)
+	const mapYDisplacement = useRef(0)
+	const mapYOffset = useRef(0)
 	
+	/**
+	 * Keeps track of the offset value, and smoothly moves
+	 * the map to fit it's position
+	 */
 	function doMomentumScrolling() {
-		mapDisplacement.current += (mapOffset.current - mapDisplacement.current) * SCROLLING_SPEED
+		mapXDisplacement.current += (mapXOffset.current - mapXDisplacement.current) * SCROLLING_SPEED
+		mapYDisplacement.current += (mapYOffset.current - mapYDisplacement.current) * SCROLLING_SPEED
   
-		const translation = `0px -${ mapDisplacement.current }px`
+		const translation = `-${ mapXDisplacement.current }px -${ mapYDisplacement.current }px`
 		map.current?.style.setProperty("translate", translation)
 	
 		requestAnimationFrame(doMomentumScrolling)
 	}
 
+	function setupScrollingLimits () {
+		if (!map.current) return // Prevent null warning
+		if (!home.current) return // Prevent null warning
+
+		topXScrollLimit.current = map.current.offsetWidth - home.current.clientWidth
+		topYScrollLimit.current = map.current.offsetHeight - home.current.clientHeight
+	}
+
+	function setupMapMovementWithMouseWheel () {
+		home.current?.addEventListener("wheel", (wheel) => {
+			if (wheel.shiftKey)
+				mapXOffset.current = Math.max( Math.min(mapXOffset.current + wheel.deltaY, topXScrollLimit.current), 0 )  
+			
+			else
+				mapYOffset.current = Math.max( Math.min(mapYOffset.current + wheel.deltaY, topYScrollLimit.current), 0 )  
+		})
+	}
+
 	useEffect(() => {
+		setupScrollingLimits()
+		setupMapMovementWithMouseWheel()
+
 		doMomentumScrolling()
 	}, [])
 
 	return <>
-		<div className="home">
+		<div ref={ home } className="home">
 			<div ref={ map } className="map">
 				<MapBackground />
 				<SparkEnvironment />
